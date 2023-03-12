dc765004ea7076913fd2858a08e355e5394d5d8b
Brahm V <brahmvanh@gmail.com>
2023-03-12 11:26:02 -0400

    Created route for searching ingredients from spoonacular

--- controllers/spoonacular/spoonacularRoutes.js
+++ controllers/spoonacular/spoonacularRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { Recipe } = require("../../models_ignored");
-const spoonacularBaseUrl = "https://api.spoonacular.com/recipes/complexSearch";
+const spoonacularBaseUrl = "https://api.spoonacular.com";
 require("dotenv").config();
 
 // User can search recipe by common name
@@ -8,10 +8,9 @@ require("dotenv").config();
 
 router.get("/:mealName", async (req, res) => {
   try {
-
-  // Using async await to request user input from spoon 
+    // Using async await to request user input from spoon
     const spoonRecipeData = await fetch(
-      `${spoonacularBaseUrl}?apiKey=${process.env.API_KEY}&query=${mealName}&addRecipeInformation=true`
+      `${spoonacularBaseUrl}/recipes/complexSearch?apiKey=${process.env.API_KEY}&query=${mealName}&addRecipeInformation=true`
     );
 
     // Information mapped and adapted to "plain" so we can render to {{{}}}
@@ -32,3 +31,26 @@ router.get("/:mealName", async (req, res) => {
 
 // Stretch: There are endless ways we can modify this search
 // i.e. 'excludeRecipe', 'intolerances', 'diet'
+
+router.get("/:ingredient", async (req, res) => {
+  try {
+    // Using async await to request user input from spoon
+    const spoonData = await fetch(
+      `${spoonacularBaseUrl}/food/ingredients/search?apiKey=${process.env.API_KEY}&query=${ingredient}&number=2&metaInformation=true`
+    );
+
+    // Information mapped and adapted to "plain" so we can render to {{{}}}
+    const spoonIngredients = spoonData.map((spoonIngredient) =>
+      spoonIngredient.get({ plain: true })
+    );
+
+    // Render spoonRecipes data to main recipe-search.handlebars
+    res.render("recipe-search", {
+      spoonIngredients,
+      // do we want this feature to require logging in?
+      // logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});

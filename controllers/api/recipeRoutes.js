9a8f18ce58792d1dfcd7057c6670c2ba59c1b905
lsieck519 <99048123+lsieck519@users.noreply.github.com>
2023-03-13 14:35:14 -0500

    tiny change to get recipe route to work

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ["user_name"],
+          attributes: ["name"],
         },
       ],
     });
